0e9ece1329e667621114cd5ab7c0d2c92c0abb0c
HyunseokLEE <unducklee@gmail.com>
2023-08-09 19:49:41 +0900

    chore: seo

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,10 +1,15 @@
 export const Seo = {
   Title: {
-    //main: 'Main',
-    intro: 'Intro',
-    //team: 'Team',
-    //work: 'Work',
-    //edu: 'Edu',
-    //notFound: 'Not Found',
+    home: 'Home',
+    intro: 'ADDS 소개',
+    member: '연구진 소개',
+    research: '조사설계',
+    publication: '발간물',
+    resource: '자료안내',
+    paper: '데이터활용논문',
+    notice: '공지사항',
+    news: '보도자료',
+    faq: 'FAQ',
+    contact: 'Contact',
   },
 } as const;
