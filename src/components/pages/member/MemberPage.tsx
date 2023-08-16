74c84e94aba7f153b20d103fbe36bb8fc0e8c02d
Hui Kang <sheekang30302@gmail.com>
2023-08-16 19:56:49 +0900

    feat: member page initial code (#3)

--- src/components/pages/member/MemberPage.tsx
+++ src/components/pages/member/MemberPage.tsx
@@ -1,3 +1,48 @@
+import styled from 'styled-components';
+import {Fonts} from '../../../styles/fonts';
+
 export const MemberPage = () => {
-  return <div>MemberPage</div>;
+  return <MemberSection>
+    <div>연구진 소개</div>
+    <MemberGridBox>
+      <div>
+        김현경 교수
+      </div>
+      <div>
+        김현경 교수
+      </div>
+      <div>
+        김현경 교수
+      </div>
+      <div>
+        김현경 교수
+      </div>
+      <div>
+        김현경 교수
+      </div>
+      <div>
+        김현경 교수
+      </div>
+      <div>
+        김현경 교수
+      </div>
+    </MemberGridBox>
+  </MemberSection>;
 };
+
+const MemberSection = styled.section`
+>div{
+  ${Fonts.regular30};
+}
+`; 
+
+const MemberGridBox = styled.div`
+display:grid;
+grid-template-columns: 1fr 1fr;
+>div{
+  border: 1px solid black;
+}
+>div:nth-child(1){
+grid-column: 1/span 2;
+}
+`; 
